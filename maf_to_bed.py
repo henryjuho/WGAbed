2c22af4b28f7bf9a97bf43be711c157f7e3cda4c
Padraic Corcoran <padraiccorcoran1@gmail.com>
2016-09-20 17:11:06 +0100

    Added coordinates for all genomes in column 7 and alignment score in last column

--- maf_to_bed.py
+++ maf_to_bed.py
@@ -40,7 +40,7 @@ def revpos(pos, seq_len, block_len):
     return reverse_pos
 
 
-def create_bed_records(aln_block, spec, ref):
+def create_bed_records(aln_block, spec, ref, score):
 
     bed_line = [aln_block[ref][0], int(aln_block[ref][1]), int(aln_block[ref][1]) + 1, aln_block[ref][3]]
     # print(bed_line)
@@ -48,12 +48,15 @@ def create_bed_records(aln_block, spec, ref):
     species_lst = []
     chroms = []
     sites = []
-    # start = []
+    positions = []
     strands = []
+
+    gap_count = {spc: 0 for spc in spec} # dictionary for holding the count of '-' characters in alignment block
 
     # print(len(aln_block[ref][5]))
     for pos in range(len(aln_block[ref][5])):
         if aln_block[ref][5][pos] == '-':
+            gap_count[ref] += 1
             continue
         for sp in spec:
             species_lst.append(sp)
@@ -62,17 +65,26 @@ def create_bed_records(aln_block, spec, ref):
                 sites.append(aln_block[sp][5][pos])
                 chroms.append(aln_block[sp][0])
                 strands.append(aln_block[sp][3])
+
+                if aln_block[sp][5][pos] == '-':
+                    positions.append('NA')
+                    gap_count[sp] += 1
+                else:
+                    positions.append(str(int(aln_block[sp][1]) + pos - gap_count[sp]))
+
             else:
                 sites.append('?')
                 chroms.append('?')
                 strands.append('?')
 
-        bed_line_str = [str(i) for i in bed_line]
+        bed_line_str = [str(s) for s in bed_line]
 
         bed_line_str.append(','.join(species_lst))
         bed_line_str.append(','.join(chroms))
+        bed_line_str.append(','.join(positions))
         bed_line_str.append(','.join(sites))
         bed_line_str.append(','.join(strands))
+        bed_line_str.append(score)
 
         print('\t'.join(bed_line_str))
 
@@ -80,6 +92,7 @@ def create_bed_records(aln_block, spec, ref):
         del species_lst[:]
         del chroms[:]
         del sites[:]
+        del positions[:]
         del strands[:]
 
         # print(bed_line)
@@ -89,7 +102,7 @@ def create_bed_records(aln_block, spec, ref):
 
 ref_species = sys.argv[2]
 
-species_list = [i.rstrip() for i in open(sys.argv[3], 'r')] # list of species to extract
+species_list = [i.rstrip() for i in open(sys.argv[3], 'r')]  # list of species to extract
 
 ref_chrom = sys.argv[4]
 
@@ -100,7 +113,7 @@ with gzip.open(sys.argv[1]) as infile:
             continue
         elif line.startswith('a'):
             # consider storing score at this stage
-            print(line.split())
+            align_score = line.split()[1].split('=')[1]
             align_block = {}
             # block_start = 1
             continue
@@ -131,13 +144,13 @@ with gzip.open(sys.argv[1]) as infile:
                         else:
                             align_block[s][3] = '-'
 
-                    # print(align_block)
+                # print(align_block)
 
-                    create_bed_records(align_block, species_list, ref_species)
+                create_bed_records(align_block, species_list, ref_species, align_score)
 
-                else:
-                    # print(align_block)
-                    create_bed_records(align_block, species_list, ref_species)
+                # else:
+                #     # print(align_block)
+                #     create_bed_records(align_block, species_list, ref_species, align_score)
 
 
 ##TODO check if the reference genomome is covered by more than one alignment in the MAF file
