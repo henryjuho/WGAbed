a82b6ddba5fca149ad0da078f4047f366bf69511
Padraic Corcoran <padraiccorcoran1@gmail.com>
2016-10-04 14:48:40 +0100

    Changed complement fucntion to use dictionary and added '?' for poition when a species is absent from an alignment block where the ref species is present

--- maf_to_bed.py
+++ maf_to_bed.py
@@ -1,25 +1,18 @@
 from __future__ import print_function
 import sys
 import gzip
+import argparse
 
 
 def complement(seq):
 
+    d = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N', '-': '-', 'a': 't', 'c': 'g', 'g': 'c', 't': 'a', 'n': 'n'}
+
     comp_seq = ''
-    for n in seq.upper():
-        if n == 'A':
-            comp_seq += 'T'
-        elif n == 'T':
-            comp_seq += 'A'
-        elif n == 'G':
-            comp_seq += 'C'
-        elif n == 'C':
-            comp_seq += 'G'
-        elif n == '-':
-            comp_seq += '-'
-        elif n == 'N':
-            comp_seq += 'N'
-        else:
+    for n in seq:
+        try:
+            comp_seq += d[n]
+        except KeyError:
             sys.exit('Unrecognised aligment character %s' % n)
 
     return comp_seq
@@ -50,7 +43,7 @@ def create_bed_records(aln_block, spec, ref, score):
     positions = []
     strands = []
 
-    gap_count = {spc: 0 for spc in spec} # dictionary for holding the count of '-' characters in alignment block
+    gap_count = {spc: 0 for spc in spec}  # dictionary for holding the count of '-' characters in alignment block
 
     # print(len(aln_block[ref][5]))
     for pos in range(len(aln_block[ref][5])):
@@ -75,6 +68,7 @@ def create_bed_records(aln_block, spec, ref, score):
                 sites.append('?')
                 chroms.append('?')
                 strands.append('?')
+                positions.append('?')
 
         bed_line_str = [str(s) for s in bed_line]
 
@@ -99,50 +93,72 @@ def create_bed_records(aln_block, spec, ref, score):
         bed_line[2] += 1
 
 
-ref_species = sys.argv[2]
 
-species_list = [i.rstrip() for i in open(sys.argv[3], 'r')]  # list of species to extract
+def main():
 
-ref_chrom = sys.argv[4]
+    parser = argparse.ArgumentParser(description="Convert a whole genome alignment in MAF format to BED format "
+                                                 "following the coordinates of one of the species in the alignment ")
+    parser.add_argument('-i', '--infile', dest='infile', required=True, help="Whole genome alignment in MAF format "
+                                                                             "(Compressed)")
+    parser.add_argument('-r', '--ref_species', dest='ref_species', required=True, help="Name of reference "
+                                                                                       "species (as it appears "
+                                                                                       "in the MAF file")
+    parser.add_argument('-s', '--species_list', dest='species_list', required=True, help="Text file listing the "
+                                                                                         "species names of the "
+                                                                                         "species in the MAF file. This"
+                                                                                         " file determines in which "
+                                                                                         "order the species are in the "
+                                                                                         "BED file")
+    parser.add_argument('-c', '--chromosome', dest='ref_chrom', required=True, help="Specify which chromosome to"
+                                                                                           "extract. This script only "
+                                                                                           "extracts one to BED format "
+                                                                                           "one chromosome in each run")
 
-with gzip.open(sys.argv[1]) as infile:
-    # align_block = {}
-    for line in infile:
-        if line.startswith('#'):
-            continue
-        elif line.startswith('a'):
-            align_score = line.split()[1].split('=')[1]
-            align_block = {}
-            continue
-        elif line.startswith('s'):
-            species = line.split()
-            align_block[species[1].split('.')[0]] = [species[1].split('.')[1]] + species[2:]
+    args = parser.parse_args()
 
-        else:
-            # block_start = 0
-            if ref_species not in align_block.keys():
+    ref_species = args.ref_species
+
+    species_list = [i.rstrip() for i in open(args.species_list, 'r')]  # list of species to extract
+
+    ref_chrom = args.ref_chrom
+
+    with gzip.open(args.infile) as infile:
+        # align_block = {}
+        for line in infile:
+            if line.startswith('#'):
+                continue
+            elif line.startswith('a'):
+                align_score = line.split()[1].split('=')[1]
                 align_block = {}
                 continue
+            elif line.startswith('s'):
+                species = line.split()
+                align_block[species[1].split('.')[0]] = [species[1].split('.')[1]] + species[2:]
+
             else:
-                ref_seq_entry = align_block[ref_species]
-                chrom = ref_seq_entry[0]
-                if chrom != ref_chrom:
+                # block_start = 0
+                if ref_species not in align_block.keys():
+                    align_block = {}
                     continue
-                ref_strand = ref_seq_entry[3]
-                # print(align_block)
-                if ref_strand == '-':
-                    for s in align_block.keys():
-                        align_block[s][1] = revpos(align_block[s][1], align_block[s][4], int(align_block[s][2]))
-                        align_block[s][5] = revcomp(align_block[s][5])
-                        if align_block[s][3] == '-':
-                            align_block[s][3] = '+'
-                        else:
-                            align_block[s][3] = '-'
+                else:
+                    ref_seq_entry = align_block[ref_species]
+                    chrom = ref_seq_entry[0]
+                    if chrom != ref_chrom:
+                        continue
+                    ref_strand = ref_seq_entry[3]
+                    # print(align_block)
+                    if ref_strand == '-':
+                        for s in align_block.keys():
+                            align_block[s][1] = revpos(align_block[s][1], align_block[s][4], int(align_block[s][2]))
+                            align_block[s][5] = revcomp(align_block[s][5])
+                            if align_block[s][3] == '-':
+                                align_block[s][3] = '+'
+                            else:
+                                align_block[s][3] = '-'
 
-                create_bed_records(align_block, species_list, ref_species, align_score)
+                    create_bed_records(align_block, species_list, ref_species, align_score)
 
-
-
-##TODO check if the reference genomome is covered by more than one alignment in the MAF file
+if __name__ == '__main__':
+    main()
 
 
